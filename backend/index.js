1359d24a19cd4b4def6311b7c5d116853aaf6674
hptut <100429932+hptut@users.noreply.github.com>
2022-09-23 11:52:56 -0400

    adding backend example
    add ur names to index.js

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var names = ["Hayden Tuttle","Name","Name","Name","Name"]; //add your names to this array
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<HTML><BODY bgcolor=#c0c0c0>');
+    res.write('<H1>Welcome to trove!</H1>')
+    res.write('<H2>created by : </H2>');
+    for(let i =0;i<names.length;i++) {
+        res.write("<H3>" + names[i] + "</H3>");
+    }
+    res.end('</BODY></HTML>');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
