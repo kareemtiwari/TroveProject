7a576d8474a3e8f15499b96217ed8be478328354
hptut <100429932+hptut@users.noreply.github.com>
2022-09-29 21:51:46 -0400

    adding express and view engines

--- trove/routes/accsettings.js
+++ trove/routes/accsettings.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' ,path: req.path});
+  res.render('index', { title: 'Account settings',path: req.path });
 });
 
 module.exports = router;
