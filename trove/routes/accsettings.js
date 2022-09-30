060c34bfc5e05dcae4dbb37c30551920f6e14823
hptut <100429932+hptut@users.noreply.github.com>
2022-09-30 19:06:38 -0400

    fixing toolbar

--- trove/routes/accsettings.js
+++ trove/routes/accsettings.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Account settings',path: req.path });
+  res.render('index', { title: 'Account settings',path: req.originalUrl });
 });
 
 module.exports = router;
--- trove/routes/index.js
+++ trove/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' ,path: req.path});
+  res.render('index', { title: 'Express' ,path: req.originalUrl});
 });
 
 module.exports = router;
