7929db8f6424c6b9cc8882c7ea49c2b3581b3a20
kareemtiwari <36958396+kareemtiwari@users.noreply.github.com>
2022-10-10 18:36:30 -0400

    Created the route for the Login page
    Created the route for the login page and added the lines to the app.js so that I could use the created routed object.

--- trove/app.js
+++ trove/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accSetRouter = require('./routes/accSettings');
-
+var LoginRouter = require('./routes/Trove_Login');
 var app = express();
 
 //Integrated Database section (Database option 1)
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //app.use('/accSettings', accSetRouter);
-
+//app.use('/Trove_Login',Trove_Login);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
