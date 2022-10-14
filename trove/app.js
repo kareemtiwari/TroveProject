2ba48875b98b1a1e895cf17a9fa868c484f886e9
kareemtiwari <36958396+kareemtiwari@users.noreply.github.com>
2022-10-14 11:38:41 -0400

    Update app.js
    Changing the name of the TroveLogin to the LoginRouter

--- trove/app.js
+++ trove/app.js
@@ -83,7 +83,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //app.use('/accSettings', accSetRouter);
-//app.use('/Trove_Login',Trove_Login);
+//app.use('/Trove_Login',LoginRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
