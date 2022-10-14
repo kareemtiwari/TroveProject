f73e146343e73fde715de287f4b7587fd3558995
kareemtiwari <36958396+kareemtiwari@users.noreply.github.com>
2022-10-14 13:39:51 -0400

    #8 checking the username and password on Login page
    Checking the username and the password and comparing with the database on submition

--- trove/routes/Trove_Login.js
+++ trove/routes/Trove_Login.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
+let accountModel = require('../db/Objects/account.js').Account;
 /* GET Login page. */
 router.get('/', function(req, res, next) {
     res.render('Trove_Login', { title: 'Express' ,path: req.originalUrl});
 });
+router.post('*', function(req, res, next) {
 
+    Userval = req.body["UsName"]
+    Passval = req.body["Psswd"]
+    geUser = getByUserName(Userval)
+     if(getUser["password"] === Passval)
+    {
+
+        res.render('Trove_Login', {nmessage: "Welcome" + accountModel.getAttributes("firstName")})
+    }
+     else{
+         res.render('Trove_Login', {nmessage: "Passwords Do NOT Match"})
+    }
+
+
+});
+
+async function getByUserName(email){
+    name = await accountModel.findAll({
+       where: {
+           email: "UsName"
+       }
+
+})
+
+
+
+}
 module.exports = router;
