4dc2a4aefac88c30a5e0155f4de95e77eab4ea16
kareemtiwari <36958396+kareemtiwari@users.noreply.github.com>
2022-10-17 19:00:43 -0400

    #5 Syntax error fixes
    Sends the tracer fully to the database as well as receives the proper response

--- trove/routes/Sign_Up.js
+++ trove/routes/Sign_Up.js
@@ -7,18 +7,22 @@ router.get('*', function(req, res, next) {
 });
 router.post('*', async function(req, res, next) {
     session = req.session
-    FirstVal = req.body["Fname"]
-    LastVal = req.body["Lname"]
-    EmailVal = req.body['Email']
-    NewPassVal = req.body['NewPsswd']
-    CheckPassVal = req.body['CheckPsswd']
-    if(FirstVal !==  "" && LastVal !== ""&& EmailVal !== "" && NewPassVal !== "" && CheckPassVal !== "" && CheckPassVal === NewPassVal){
-        await accountModel.create({firstname:FirstVal, lastName:LastVal, Email:EmailVal, password:NewPassVal});
-        res.redirect('/accSettings')
-
+    FirstVal = req.body["Fname"];
+    LastVal = req.body["Lname"];
+    EmailVal = req.body["Email"];
+    NewPassVal = req.body["NewPsswd"];
+    CheckPassVal = req.body["CheckPsswd"];
+    if(FirstVal !==  "" && LastVal !== ""&& EmailVal !== "" && NewPassVal !== "" && CheckPassVal !== "" ){
+        if(CheckPassVal === NewPassVal){
+            await accountModel.create({firstName:FirstVal, lastName:LastVal, email:EmailVal, password:NewPassVal});
+            res.redirect('/accSettings')
+            return;
+        }
 
     }
 
+    console.log(NewPassVal,CheckPassVal);
+    res.render('Sign-Up', {nmessage:"Error"});
 
 });
 module.exports = router;
