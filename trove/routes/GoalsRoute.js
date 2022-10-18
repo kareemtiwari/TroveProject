6dda140d6a01694d1b01abdc63a44c2477aebaf1
austonmain <austonmain@gmail.com>
2022-10-18 10:03:05 -0400

    Database still throws null values
    The database returns the one value it needs to for this milestone but its still not allowing any of the other values.

--- trove/routes/GoalsRoute.js
+++ trove/routes/GoalsRoute.js
@@ -7,17 +7,17 @@ let goalModel = require('../db/Objects/dbGoals.js').DbGoals;   //NEEDED TO USE D
  * GET router - this is what is called when this routers path is hit with an HTTP get request
  * This usually happens when a user navigated to your page, or refreshes the page
  */
-router.get('/',  function(req, res, next) {
-    // let uid = req.session.userID;
-    // //TODO : have to check if there is a userID in the session
-    // //get the currently logged in user
-    // let query = await goalModel.findAll({
-    //     where: {
-    //         id: uid
-    //     },
-    //     raw : true
-    // });
-    // let user = query[0];  //the first user in query - there should really only ever be 1
+router.get('*',  async function(req, res, next) {
+    let uid = req.session.userID;
+    //TODO : have to check if there is a userID in the session
+    //get the currently logged in user
+    let query = await goalModel.findAll({
+        where: {
+            id: 1 //was uid
+        },
+        raw : true
+    });
+    let user = query[0];  //the first user in query - there should really only ever be 1
     // //TODO : have to check if there is a user
     res.render('Goals', {path: req.originalUrl}); //TODO : model doesn't have all
     // console.log(user.id);
@@ -27,7 +27,7 @@ router.get('/',  function(req, res, next) {
  * POST router - this is what is called when this routers path is hit with an HTTP post request
  * This usually happens when a user has clicked submit on a form, or is otherwise sending data to your site
  */
-router.post('/', async function(req, res, next) {
+router.post('*', async function(req, res, next) {
     console.log(req.url);
     console.log(req.body);
 
@@ -35,11 +35,11 @@ router.post('/', async function(req, res, next) {
     uid = req.session.userID; //need to check if there is one - [also eventually need to check if they are being brute forced??]
     let gID = req.body["goalID"];  //get all variables out of the form
 
-    var gAmount = req.body["goalAmount"];
+    let gAmount = req.body["goalAmount"];
     gAmount = parseInt(gAmount);
     var gProgress = req.body["goalProgress"];
     gProgress = parseInt(gProgress);
-    let gName = req.body["goalName"];
+    var gName = req.body["goalName"];
     var gSlider = req.body["goalSlider"];
 
 
@@ -48,10 +48,10 @@ router.post('/', async function(req, res, next) {
     //     res.render('Goals', {remessage: 'Goal Name must be 1-15 Letters only', path: req.originalUrl});
     //     return;
     // }
-    newGoal = goalModel.create({userID: 1, goalID: gID, goalAmount: gAmount, goalProgress: gProgress, goalName: gName,goalSlider: gSlider});
+    newGoal = goalModel.create({userID: 1, goalID: gID, goalAmount: gAmount, goalProgress: 10, goalName: "car",goalSlider: 50});
         let query = await goalModel.findAll({raw:true});
         console.log(query);
-        console.log("***Goal***"+gID+" Created");
+        console.log("***Goal***"+gAmount+" Created");
     //,{where:{userID:uid}}
         //res.redirect('/Dashboard');
     //}else{
