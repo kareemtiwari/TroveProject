28ddef865806d40c25d741c14155d98aae258e58
jonahfullen <jwf5164@uncw.edu>
2022-10-31 18:37:33 -0400

    Restructure Post
    Restructure post method to a single switch statement.

--- trove/routes/WeeklyCalendar.js
+++ trove/routes/WeeklyCalendar.js
@@ -22,7 +22,7 @@ router.get('/', async function(req, res, next) {
         tue:dispList[2], wed:dispList[3], thu:dispList[4], fri:dispList[5], sat:dispList[6], path: req.originalUrl});
 });
 
-router.post('/addEvent', async function(req, res, next) {
+router.post('*', async function(req, res, next) {
     let query = await eventsModel.findAll({
         where: {
             userID: 0
@@ -35,50 +35,59 @@ router.post('/addEvent', async function(req, res, next) {
     let eventsList = getEventsList(query);
     console.log(eventsList);
     let dispList = getDsiplayList(eventsList);
-    /* Get and Check the New Event Name */
-    let eName = req.body["eName"];
-    if(eName.length === 0){
-        res.render('WeeklyCalendar', {name: 'Event must be named.', end:'', wage:'', sun:dispList[0],
-            mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4], fri:dispList[5],
-            sat:dispList[6], path: req.originalUrl});
-        return;
+    
+    let type = req.body["type"];
+    switch (type) {
+        case 'addEvent':
+            /* Get and Check the New Event Name */
+            let eName = req.body["eName"];
+            if(eName.length === 0){
+                res.render('WeeklyCalendar', {name: 'Event must be named.', end:'', wage:'', sun:dispList[0],
+                    mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4], fri:dispList[5],
+                    sat:dispList[6], path: req.originalUrl});
+                return;
+            }
+
+            /* Get the New Event Day */
+            var eDay = req.body["eDay"];
+            eDay = parseInt(eDay);
+
+            /* Get the New Event Start Time */
+            let eStart = req.body["eStart"];
+            eStart = parseFloat(eStart.replace(",","."));
+
+            /* Get and Check the New Event End Time */
+            let eEnd = req.body["eEnd"];
+            eEnd = parseFloat(eEnd.replace(",","."));
+            if(eEnd <= eStart){
+                res.render('WeeklyCalendar', {name: '', end:'Event end time must be after the start time.',
+                    wage:'', sun:dispList[0], mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4],
+                    fri:dispList[5], sat:dispList[6], path: req.originalUrl});
+                return;
+            }
+
+            /* Get and Check the New Event Hourly Wage */
+            let eWage = req.body["eHourly"];
+            eWage = parseFloat(eWage.replace(",","."));
+            if(isNaN(eWage)) {
+                res.render('WeeklyCalendar', {name: '', end:'', wage:'Hourly Wage must be a valid number.',
+                    sun:dispList[0], mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4], fri:dispList[5],
+                    sat:dispList[6],path: req.originalUrl});
+                return;
+            }
+
+
+            newEvent = eventsModel.create({eventID:0, userID:0, calendarID:0, eventName:eName, eventDay:eDay,
+                eventStartTime:eStart, eventEndTime:eEnd, eventWage:eWage});
+
+            console.log(query);
+            console.log("***New Event " + eName + " Created***");
+            res.redirect("/Weekly-Calendar");
+            break;
+        case 'editEvent':
+            // CODEEEEE
     }
 
-    /* Get the New Event Day */
-    var eDay = req.body["eDay"];
-    eDay = parseInt(eDay);
-
-    /* Get the New Event Start Time */
-    let eStart = req.body["eStart"];
-    eStart = parseFloat(eStart.replace(",","."));
-
-    /* Get and Check the New Event End Time */
-    let eEnd = req.body["eEnd"];
-    eEnd = parseFloat(eEnd.replace(",","."));
-    if(eEnd <= eStart){
-        res.render('WeeklyCalendar', {name: '', end:'Event end time must be after the start time.',
-            wage:'', sun:dispList[0], mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4],
-            fri:dispList[5], sat:dispList[6], path: req.originalUrl});
-        return;
-    }
-
-    /* Get and Check the New Event Hourly Wage */
-    let eWage = req.body["eHourly"];
-    eWage = parseFloat(eWage.replace(",","."));
-    if(isNaN(eWage)) {
-        res.render('WeeklyCalendar', {name: '', end:'', wage:'Hourly Wage must be a valid number.',
-            sun:dispList[0], mon:dispList[1], tue:dispList[2], wed:dispList[3], thu:dispList[4], fri:dispList[5],
-            sat:dispList[6],path: req.originalUrl});
-        return;
-    }
-
-
-    newEvent = eventsModel.create({eventID:0, userID:0, calendarID:0, eventName:eName, eventDay:eDay,
-        eventStartTime:eStart, eventEndTime:eEnd, eventWage:eWage});
-
-    console.log(query);
-    console.log("***New Event " + eName + " Created***");
-    res.redirect("/Weekly-Calendar");
 });
 
 function getEventsList(query) {
