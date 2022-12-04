8bce0e441fe3ac2ff9e97fb8482dbbb1815247c1
austonmain <austonmain@gmail.com>
2022-12-04 16:04:00 -0500

    goalTesting progress
    #80

--- trove/test-scripts/goals.test.js
+++ trove/test-scripts/goals.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3000";
 
-describe("Testing the add goal info", () => {
+describe("Testing the creation of a goal", () => {
     test("It should respond by redirect", async () => {
         const form = {
             'goalID': 'option1',
@@ -9,6 +9,26 @@ describe("Testing the add goal info", () => {
             'goalProgress': '20',
             'goalName': 'New car',
             'goalSlider': '50'
+        };
+        const response = await request(baseURL).post("/TroveAccounting/").send(form);
+        expect(response.statusCode).toBe(301);
+
+    });
+});
+describe("Testing the deletion of a goal", () => {
+    test("It should respond by redirect", async () => {
+        const form = {
+            'goalID': 'option1',
+        };
+        const response = await request(baseURL).post("/TroveAccounting/").send(form);
+        expect(response.statusCode).toBe(301);
+
+    });
+});
+describe("Testing the add of funds", () => {
+    test("It should respond by redirect", async () => {
+        const form = {
+            'goalID': 'option1',
         };
         const response = await request(baseURL).post("/TroveAccounting/").send(form);
         expect(response.statusCode).toBe(301);
